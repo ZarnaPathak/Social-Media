3510ccc6d83c3710d8bc65d8bb08b09f89350c83
Zarna Pathak <pathakzarna0128@gmail.com>
2023-07-02 20:25:03 +0530

    Use of React Firebase Hooks
    Installed package react-firebase-hooks, used "useAuthState" Hook.

--- socialMedia/src/components/navbar.tsx
+++ socialMedia/src/components/navbar.tsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const Navbar = () => {
+  const [user] = useAuthState(auth);
   return (
     <>
       <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;
       <Link to="/login">Login</Link>
-      <h4>{auth.currentUser?.displayName}</h4>
-      <img src={auth.currentUser?.photoURL || "not set"} />
+      <h4>{user?.displayName}</h4>
+      <img src={user?.photoURL || "not set"} alt="Profile"/>
     </>
   );
 };
