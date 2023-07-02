2f089d2014ed5c9500652183a61f4deff31a4da3
Zarna Pathak <pathakzarna0128@gmail.com>
2023-07-02 20:39:18 +0530

    Added Sign Out functionality

--- socialMedia/src/components/navbar.tsx
+++ socialMedia/src/components/navbar.tsx
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
 import { auth } from "../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 export const Navbar = () => {
   const [user] = useAuthState(auth);
+  const signOutUser = async () => {
+    await signOut(auth);
+  };
   return (
     <>
       <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;
       <Link to="/login">Login</Link>
-      <h4>{user?.displayName}</h4>
-      <img src={user?.photoURL || "not set"} alt="Profile"/>
+      {user && (
+        <>
+          <h4>{user?.displayName}</h4>
+          <img src={user?.photoURL || "not set"} alt="Profile" />
+          <div>
+            <button onClick={signOutUser}>Log Out</button>
+          </div>
+        </>
+      )}
     </>
   );
 };
