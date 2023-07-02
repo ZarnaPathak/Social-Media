ff18a52ab5bcf3b741b2cfd0f4f2bef5ea3de077
Zarna Pathak <pathakzarna0128@gmail.com>
2023-07-02 18:44:30 +0530

    Created Routes for the App
    Installed package for routes and created directory for pages.

--- socialMedia/src/App.tsx
+++ socialMedia/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <div>
-        
+        <Router>
+          <Routes>
+            <Route path='/'/>
+          </Routes>
+        </Router>
       </div>
       <h1>Hiii</h1>
     </>
