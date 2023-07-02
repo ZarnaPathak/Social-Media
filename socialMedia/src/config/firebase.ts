5927c4265c45b89a2d8bb631b1a0620a8632ef0f
Zarna Pathak <pathakzarna0128@gmail.com>
2023-07-02 19:25:28 +0530

    Set Up Firebase
    Created account on firebase official website, Registered App into it, Created "Config" directory and a file for firebase setup.

---
+++ socialMedia/src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMjc7ic6wg2jP8qbU95vJOrTSBHRAoLqM",
+  authDomain: "social-media-2ea94.firebaseapp.com",
+  projectId: "social-media-2ea94",
+  storageBucket: "social-media-2ea94.appspot.com",
+  messagingSenderId: "974973108547",
+  appId: "1:974973108547:web:6831c56295386687092ff6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
