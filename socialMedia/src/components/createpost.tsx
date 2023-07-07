ed89348c63737eba736994e27adec92ed930db05
Zarna Pathak <pathakzarna0128@gmail.com>
2023-07-07 23:27:46 +0530

    Validations Added into CreatePost form
    with the use of "useForm", "yupResolver" and "Yup".

--- socialMedia/src/components/createpost.tsx
+++ socialMedia/src/components/createpost.tsx
@@ -1,4 +1,30 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useNavigate } from "react-router-dom";
+
+interface postData {
+  title: string;
+  discription: string;
+}
+
 export const CreatePost = () => {
+  const schema = yup.object().shape({
+    title: yup.string().required("You should Add Title"),
+    discription: yup.string().required("You should Add Discription"),
+  });
+
+  const { register, handleSubmit, formState:{errors} } = useForm<postData>({
+    resolver: yupResolver(schema),
+  });
+
+  const navigate = useNavigate();
+
+  const createPostForm = (data: postData) => {
+    console.log(data);
+    navigate("/");
+  };
+
   return (
     <>
       <br />
@@ -10,14 +36,16 @@ export const CreatePost = () => {
         >
           <h2 className="card-title font-monospace">Write Your Post</h2>
           <div className="card-body">
-            <form>
+            <form onSubmit={handleSubmit(createPostForm)}>
               <div className="form-outline mb-4">
                 <p className="form-label fs-5 text-start">Title</p>
                 <input
                   type="text"
                   id="form4Example1"
                   className="form-control"
+                  {...register("title")}
                 />
+                <p style={{color:"red"}}>{errors.title?.message}</p>
               </div>
 
               <div className="form-outline mb-4">
@@ -25,9 +53,15 @@ export const CreatePost = () => {
                 <textarea
                   className="form-control"
                   id="form4Example3"
+                  {...register("discription")}
                 ></textarea>
+                <p style={{color:"red"}}>{errors.discription?.message}</p>
               </div>
-              <button className="btn btn-secondary">Post Me!</button>
+              <input
+                type="submit"
+                className="btn btn-secondary"
+                value="Post Me!"
+              />
             </form>
           </div>
         </div>
